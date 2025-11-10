d42d43d73b630734f338b762c76648ad46b62fc6
Mahima Koul <mahimakoul64@gmail.com>
2025-11-10 22:32:36 +0530

    added some deleted files

---
+++ backend/models/Item.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const itemSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    category: { type: String, enum: ["Lost", "Found"], required: true },
+    location: { type: String, required: true },
+    date: { type: Date, required: true },
+    contactInfo: { type: String, required: true },
+    image: { type: String, default: "" },
+    status: { type: String, enum: ["Open", "Resolved"], default: "Open" },
+    postedBy: { type: String, default: "Anonymous" }, // or user reference later
+  },
+  { timestamps: true }
+);
+
+const Item=mongoose.model('item', itemSchema)
+export default Item
